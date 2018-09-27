845baf0a884658a4c1e24d94da84e22184044a0f
Louis-Saglio <louis.saglio@sfr.fr>
2018-09-27 22:30:46 +0200

    Add employees resource

---
+++ controllers/employees.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const Employees = require('./../models/employees')
+
+const router = express.Router();
+
+Employees.init()
+
+router.get('/', async (req, res) => {
+    const restos = await Employees.all();
+    res.json(restos);
+})
+
+router.get('/:id', async (req, res) => {
+    const resto = await Employees.get(req.params.id);
+    if (resto === null) {
+        res.sendStatus(404)
+    } else {
+        res.json(resto)
+    }
+})
+
+router.post('/', (req, res) => {
+    const {name, restaurant_id} = req.body
+    new Employees(name, restaurant_id).insert().then(() => {res.sendStatus(201)}).catch(() => {res.sendStatus(500)}) // todo reflect in restaurants
+})
+
+router.delete('/:id', (req, res) => {
+    Employees.delete(req.params.id)
+    res.sendStatus(204)
+})
+
+router.put('/:id', async (req, res) => {
+    const employee = await Employees.get(req.params.id);
+    if (employee == undefined) {
+        res.sendStatus(404)
+    } else {
+        const {name, restaurant_id} = req.body
+        employee.name = (name == undefined ? employee.name : name)
+        employee.restaurant_id = (restaurant_id == undefined ? employee.restaurant_id : restaurant_id)
+        employee.update()
+        res.sendStatus(204)
+    }
+    
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const restoRouter = require('./controllers/restaurants')
+const employeeRouter = require('./controllers/employees')
 const bodyParser = require('body-parser')
 
 const app = express()
@@ -9,5 +10,6 @@ app.use((req, res, next) => {
     console.log('REQUEST: ' + req.method + ' ' + req.originalUrl)
 });
 app.use('/restaurants', restoRouter)
+app.use('/employees', employeeRouter)
 
 app.listen(5000, () => {console.log('running')})
