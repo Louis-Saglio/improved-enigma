b939e4df7394fa5e4a3183d48a1188f0a8219ecc
Louis-Saglio <louis.saglio@sfr.fr>
2018-10-02 17:25:12 +0200

    Use sequelize for Employees

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,52 +1,40 @@
 const express = require('express')
-const Employees = require('./../models/employees')
+// const Employees = require('./../models/employees')
+const {Restaurants, Employees} = require('./../orm/restaurants')
 
 const router = express.Router();
 
-Employees.init()
+// Employees.init()
 
 router.get('/', async (req, res) => {
-    const restos = await Employees.all();
-    res.json(restos);
+    res.json(await Employees.findAll())
 })
 
 router.get('/:id', async (req, res) => {
-    const resto = await Employees.get(req.params.id);
-    if (resto === null) {
+    const resto = await Employees.findById(req.params.id)
+    if (resto === null) {   
         res.sendStatus(404)
     } else {
         res.json(resto)
     }
 })
 
-router.post('/', (req, res) => {
-    const {name, restaurant_id} = req.body
-    new Employees(name, restaurant_id).insert()
-    .then(() => {
-        res.sendStatus(201)
-    })
-    .catch(() => {
-        res.sendStatus(500)
-    }) // todo reflect in restaurants
+router.post('/', async (req, res) => {
+    Employees.build(req.body).save()
+    res.sendStatus(201)
 })
 
 router.delete('/:id', (req, res) => {
-    Employees.delete(req.params.id)
+    Employees.destroy({where: {id: req.params.id}})
     res.sendStatus(204)
 })
 
 router.put('/:id', async (req, res) => {
-    const employee = await Employees.get(req.params.id);
-    if (employee == undefined) {
-        res.sendStatus(404)
-    } else {
-        const {name, restaurant_id} = req.body
-        employee.name = (name == undefined ? employee.name : name)
-        employee.restaurant_id = (restaurant_id == undefined ? employee.restaurant_id : restaurant_id)
-        employee.update()
-        res.sendStatus(204)
-    }
-    
+    Employees.update(
+        req.body,
+        {where: {id: req.params.id}}
+    )
+    res.sendStatus(204)    
 })
 
 module.exports = router
